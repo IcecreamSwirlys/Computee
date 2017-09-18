0c75414ee805ff5cf89d30aa16876a5539dac880
IcecreamSwirlys <e652@hotmail.ca>
2017-09-17 21:59:41 -0600

    Add files via upload

---
+++ Homepage.js
@@ -0,0 +1,15 @@
+function Adhese() {
+	var navbar = document.getElementById("navbar");
+
+	navbar.classList.toggle("navbar-stuck");
+}
+
+// var mn = $(".navbar");
+
+// $(window).scroll(function() {
+// 	if ( $(this).scrollTop() > 250) {
+// 		mn.addClass("navbar-stuck");
+// 	} else {
+// 		mn.removeClass("navbar-stuck");
+// 	}
+// });
