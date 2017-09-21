a7acdff63b80d97c696c1194061b913657b064bb
IcecreamSwirlys <e652@hotmail.ca>
2017-09-21 13:38:00 -0600

    Started to work with ScrollMagic
    ScrollMagic: http://scrollmagic.io/

--- Homepage.js
+++ Homepage.js
@@ -4,12 +4,36 @@ function Adhese() {
 	navbar.classList.toggle("navbar-stuck");
 }
 
-// var mn = $(".navbar");
 
-// $(window).scroll(function() {
-// 	if ( $(this).scrollTop() > 250) {
-// 		mn.addClass("navbar-stuck");
-// 	} else {
-// 		mn.removeClass("navbar-stuck");
-// 	}
-// });
+
+
+
+// init controller
+var controller = new ScrollMagic.Controller();
+
+// create a scene
+new ScrollMagic.Scene({
+        duration: 0,    // the scene should last for a scroll distance of 100px
+        offset: 100        // start this scene after scrolling for 50px
+    })
+    .setPin(".my-sticky-element") // pins the element for the the scene's duration
+    .addTo(controller); // assign the scene to the controller
+
+
+
+
+
+
+
+
+
+ var mn = $(".navbar");
+
+
+ $(window).scroll(function() {
+	if ( $(this).scrollTop() > 250) {
+		mn.addClass("navbar-stuck");
+	} else {
+		mn.removeClass("navbar-stuck");
+	}
+});
